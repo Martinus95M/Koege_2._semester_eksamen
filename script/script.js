1b6f3d11180100dc8e1be2bfd6dbf7f78d1f7be9
Martinus95M <martinus.mortensen@gmail.com>
2018-05-29 11:50:16 +0200

    footer, social icons, kommentar

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
    // jQuery methods go here...
+   //Dette er en toggle, dette gør at når man klikker på burger-menu icon oppe i højre hjørne vil den "show" eller "hide" efter om den er "show" eller "hide".
    $('.menu-toggle').click(function() {
 
   $('.site-nav').toggleClass('site-nav--open', 500);
